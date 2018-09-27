b584567a283d3e32334a99cee7b71831d4ea17c9
Fazzani <tunisenheni@gmail.com>
2018-09-27 23:10:39 +0200

    daily

---
+++ scripts/check_channels.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#______________________________________________________________________________
+#
+# Check si un une chaine dans le xmltv généré n'aurait pas des programmes
+# si oui on envoie un mail 
+# on met à jour le fichier utilisé par l'application web qui contient la liste de références des chaines ayant des EPG
+#______________________________________________________________________________
+
+source $(dirname $0)/utils.sh
+
+# Check if command installed
+command -v xmllint >/dev/null 2>&1 || { echoError "libxml2-utils required but it's not installed.  Aborting." >&2; exit 1; }
+command -v xml2json >/dev/null 2>&1 || { echoError "The package npm xml2json-cli required but it's not installed.  Aborting." >&2; exit 1; }
+
+# arg0: input file
+# arg1: output fie
+function checkForNewChannels {
+	echo -e  "${NC}"
+	echoInfo "__________ Cheacking epg file for new channels =>  $1 to $2"
+	echo
+
+	if [ ! -z "$1" ]; then
+		fileInput="$1"
+	else
+		echoError "no fileinput detected"
+		fileInput="guide.xmltv"
+	fi
+
+	echo 'Extract all channels from Xmltv file'
+        tmp="tmp"
+        echo "<tv>" > $tmp
+        xmllint --encode utf8 --xpath '//channel' $1 >> $tmp
+        echo "</tv>" >> $tmp
+        country=${1#*_}
+        country=${country%.xmltv}
+        xmlstarlet ed -i "//channel" -t attr -n "country" -v "$country" "$tmp" > "$tmp.xml"
+
+        xmllint --encode utf8 --xpath '//channel' "$tmp.xml" >> "$2"
+        rm tmp*
+	echo -e  "${NC}"
+}
+
+#__________________________  main __________________________
+# echo "____ : $1 arg count ____ $# : ____"
+if [ -z "$1" ];then
+	echoInfo "You must pass a list of epg files!!"
+	exit -1
+fi
+
+outputfile="check_channels.xml"
+outputfile_json="check_channels.json"
+echo "" > $outputfile #vider le fichier output
+#convert encoding to utf-8
+echo -ne '\xEF\xBB\xBF' > $outputfile
+#file -i $outputfile
+echo '<?xml version="1.0" encoding="UTF-8"?><tv generator-info-name="WebGrab+Plus/w MDB &amp; REX Postprocess -- version  V2.0 -- Jan van Straaten" generator-info-url="http://www.webgrabplus.com">' >> $outputfile
+ 
+for i in "$@";
+do
+    checkForNewChannels $i $outputfile &
+	wait
+done
+echo '</tv>' >> $outputfile
+
+# Generate json version
+
+xml2json $outputfile $outputfile_json
+
+cat $outputfile_json | jq -r '.tv.channel[] | [ .id, .url, .icon.src ] | @tsv' | \
+awk -v FS="," 'BEGIN{print "|ID\t|Url\t|Icon|";print "|:----|:---:|:---:|"}{printf "|%s\t|%s\t|%s|",$1,$2,$3}' >  channels.md
+
+#curl \
+#-H "Accept: application/json" \
+#-H "Content-Type:application/json" \
+#-X POST --data @<( cat $outputfile_json ) https://api.myjson.com/bins 
+
+# Push to Git
+
+git remote add origin2 https://${GITHUB_API_TOKEN}@github.com/fazzani/grab.git > /dev/null 2>&1
+git add $outputfile $outputfile_json channels.md && git commit -m "check channels"
+
+
+echo -e  "The End.${NC}"
+exit 0
---
+++ scripts/utils.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+APP_TOKEN="a1zc9d81aw14ezws414n7uvsnz2xio"
+USER_TOKEN="uxepp2gjx5ch4eveufj8fo8jmcm6we"
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function echoInfo {
+        echo -e "${GREEN}$1${NC}"
+}
+
+function echoError {
+        echo -e "${RED}$1${NC}"
+}
+
+#----------------------------------------
+# Auto connect SSH take passphrase as arg
+#----------------------------------------
+function ssh-auto-connect {
+  command -v expect >/dev/null 2>&1 || { echo "expect command required but it's not installed.  Aborting." >&2; exit 1; }
+
+if [ $# -ne 1 ]; then
+  echo "Usage: autoConnectSSH password"
+  exit 1
+fi
+eval $(ssh-agent)
+
+expect << EOF
+  spawn ssh-add
+  expect "Enter passphrase"
+  send "$1\r"
+  expect eof
+EOF
+}
+
+function push_msg {
+  message="${0} grab finished successfully"
+  sudo wget https://api.pushover.net/1/messages.json --post-data="token=${APP_TOKEN}&user=${USER_TOKEN}&title=WebGrabber+message&message=$message."  -qO-  > /dev/null 2>>4 &
+}
+
+function hash {
+  echo `/bin/echo $0 | /usr/bin/md5sum | cut -f1 -d" "`
+}
+
+# args <message> [title]
+function push_message {
+  if [ $# -eq 0 ]; then
+	echo "Usage: ./pushover <message> [title]"
+	exit
+  fi
+
+  MESSAGE=$1
+  TITLE=$2
+
+  if [ $# -lt 2 ]; then
+	TITLE="`whoami`@${HOSTNAME}"
+  fi
+
+  wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$MESSAGE&title=$TITLE" -qO- > /dev/null 2>&1 &
+}
+
