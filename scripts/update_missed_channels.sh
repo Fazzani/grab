886e4f076df695df8569a68042da059ddd38e571
Heni Fazzani <heni.fazzani@seloger.com>
2019-01-15 23:20:32 +0100

    fixes

--- scripts/update_missed_channels.sh
+++ scripts/update_missed_channels.sh
@@ -10,11 +10,18 @@ input_file=$1
 missed_channels_file=$2
 tmpfile="tmpppp.json"
 
-IFS=$'\n'     # make newlines the only separator
+# adding the filed active with true as default value
+jq '.tv.channel[] += {"active": true}' $input_file > $tmpfile && mv $tmpfile $input_file
+
+# make newlines the only separator
+IFS=$'\n'    
 
 for missed in $(jq -r '.sources[].missedlist[]' $missed_channels_file); do
   echo $missed
   jq -r --arg v $missed '(.tv.channel[] | select(.id == $v) | .active) |= false' $input_file > $tmpfile && mv $tmpfile $input_file
 done
+
+# updating elastic doc
+curl -s -H "Content-Type:application/json" -XPUT elastic.synker.ovh/epg/channels/9L-OU2gBt2zDT9FgG-_g -d @./out/check_channels.json
 
 exit 0
