19ae3a55608c4caf6f16a87cae3e3aca738e3138
GitHub Action <action@github.com>
2020-07-09 00:35:23 +0000

    check channels and update readme

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 OLDIFS=$IFS
 
 function build {
+    git clone --depth 1 https://${GITHUB_API_TOKEN}@github.com/fazzani/grab.git > /dev/null 2>&1 && \
+    cd grab && \
     docker run -it --rm -v "${PWD}/${WEBGRAB_FILENAME}:/config/WebGrab++.config.xml" --hostname test --mac-address="12:34:de:b0:6b:61" -v "${PWD}:/data" synker/webgraboneshot:latest
-    git remote add origin2 https://${GITHUB_API_TOKEN}@github.com/fazzani/grab.git > /dev/null 2>&1
     git add --all && (git commit -m "Webgrab ${WEBGRAB_FILENAME}" || echo "No changes to commit" && exit 0)
     git config --global http.postBuffer 524288000
 
     counter=0
     until [ $counter -gt 5 ]; do
-        git pull origin2 master &> /dev/null && git push origin2 HEAD:master -f && break || ((counter++))
-        echo Tansfer disrupted, retrying in 10 seconds...
+        { { git pull &> /dev/null && git push -f; } && break; } || ((counter++))
+        echo Transfer disrupted, retrying in 10 seconds...
         sleep 10
     done
 }
@@ -31,8 +32,8 @@ function merge {
 
     counter=0
     until [ $counter -gt 5 ]; do
-        git pull -X ours > /dev/null 2>&1 && git push -f && break || ((counter++))
-        echo Tansfer disrupted, retrying in 10 seconds...
+        { { git pull &> /dev/null && git push -f; } && break; } || ((counter++))
+        echo Transfer disrupted, retrying in 10 seconds...
         sleep 10
     done
 }
