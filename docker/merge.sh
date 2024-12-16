aa0fdc5954a84b1200da040a7335b8d81ed46e6b
Fazzani <heni.fazzani@gmail.com>
2024-12-16 15:56:58 +0100

    Fixes

--- docker/merge.sh
+++ docker/merge.sh
@@ -6,48 +6,14 @@ command -v tv_merge >/dev/null 2>&1 || { echo >&2 "I require tv_merge but it's n
 command -v gzip >/dev/null 2>&1 || { echo >&2 "I require gzip but it's not installed. Aborting."; exit 1; }
 command -v zip >/dev/null 2>&1 || { echo >&2 "I require zip but it's not installed. Aborting."; exit 1; }
 
-output="merge.xmltv"
-# i=0
-verbose=${VERBOSE:-false}
-
-# [[ $verbose = true ]] && echo "pattern to match $1"
-
-# for filename in "$1"/*.xml; do
-
-#   [[ $verbose = true ]] && echo "filename => $filename" 
-
-#   [[ $(grep -c "<programme" "$filename") -eq 0 ]] && echo "no programs" && continue
-
-#   if [[ "$filename" = "merge.xml" ]]; then
-#     continue
-#   fi
-
-#   if [[ $i -eq 0 ]]; then
-#     second=$filename
-#     i=$((i+1))
-#     continue
-#   fi
-
-#   if [[ $i -gt 1 ]]; then
-#     second=$output
-#   fi
-
-#   [[ $verbose = true ]] && echo "filename => $filename will be merged with $second" 
-
-#   echo -e "merge ${filename} $second"
-#   /usr/bin/tv_merge -i "$filename" -m $second -o $output
-#   i=$((i+1))
-
-# done;
-
+echo "Processing..................." 
 /usr/bin/tv_cat --utf8 "$1"/*.xml | /usr/bin/tv_grep --on-after now > ./merge.xml
 
-# [[ ! -f $output ]] && echo "$output file not generated!" && exit 0
-
-# /usr/bin/tv_grep --on-after now $output > ./merge.xml
-
+echo "Cleaning up.................." 
 [[ -f "./merge.zip" ]] && rm "./merge.zip"
 [[ -f "./merge.xml.gz" ]] && rm "./merge.xml.gz"
+
+echo "Archiving...................."
 zip -r merge.zip merge.xml
 gzip -fkq merge.xml
 
